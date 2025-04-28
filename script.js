306c2231c6f7cc589b4ce3aeaa1653a647654bf4
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-28 22:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
 //your JS code here. If required.
+
+
+let num=document.getElementById("ip").value;
+let btn=document.getElementById("btn")
+
+
+function myPromise1(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(num);
+		},2000)
+	})
+}
+
+function myPromise2(data){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(data*2);
+		},2000)
+	})
+}
+
+function myPromise3(data){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(data-3);
+		},1000)
+	})
+}
+
+function myPromise4(data){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(data/2);
+		},1000)
+	})
+}
+
+function myPromise5(data){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(data+10);
+		},1000)
+	})
+}
+
+
+let opt=document.getElementById("output")
+
+btn.addEventListener('click', function() {
+  let num = Number(document.getElementById("ip").value); // Get fresh input value on click
+
+  myPromise1(num)
+  .then((res)=>{
+	  opt.textContent=res;
+	  return myPromise2(res);
+  }).then((res)=>{
+	  opt.textContent=res;
+	  return myPromise3(res);
+  }).then((res)=>{
+	  opt.textContent=res;
+	  return myPromise4(res);
+  }).then((res)=>{
+	  opt.textContent=res;
+	  return myPromise5(res);
+  }).then((res)=>{
+	  opt.textContent=res;
+  })
+   
+});
+
